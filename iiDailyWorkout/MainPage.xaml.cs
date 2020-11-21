662a05fa6e03a2780e4e9a67f2a3436511cba9f0
Isaac Ingles <isaac.ingles@gmail.com>
2020-11-21 16:55:58 -0700

    Add project files.

---
+++ iiDailyWorkout/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.IO;
+
+namespace iiDailyWorkout
+{
+    public partial class MainPage : ContentPage
+    {
+        // Overall workout minutes
+        int workoutMinutes = 0;
+        // Create empty storedFile string
+        string storedFile = "";
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Display current date
+            string curDate = DateTime.Now.ToShortDateString();
+            LblDate.Text = $"Date: {curDate}";
+
+            string fileDate = curDate.Replace('/', '_');
+
+            // Load the saved file, pass date in as parameter
+            LoadSavedFile(fileDate);
+
+            LblResults.Text = workoutMinutes.ToString();
+        }
+
+        /// <summary>
+        ///     Load a saved file (should be cross-platform)
+        /// </summary>
+        /// <param name="fileDate"></param>
+        private void LoadSavedFile(string fileDate)
+        {
+            // Get and store the path
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // Store the file name by concatenating the name with the date
+            string fileName = $"{fileDate}wom.txt";
+
+            // Get the stored file path
+            storedFile = Path.Combine(docPath, fileName);
+
+            // Check out if the file exists
+
+            if (File.Exists(storedFile))
+            {
+                // Parse the file text as an integer and store it
+                workoutMinutes = int.Parse(File.ReadAllText(storedFile).ToString());
+            }
+            else
+            {
+                // if the file doesn't exist, create it
+                File.WriteAllText(storedFile, workoutMinutes.ToString());
+            }
+        }
+
+        /// <summary>
+        ///     Add minutes to file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnAddMinutes_Clicked(object sender, EventArgs e)
+        {
+            // Make sure something is selected and display an alert if nothing is selected
+            if (PckMinutes.SelectedIndex > -1)
+            {
+                // Increase workout minutes
+                workoutMinutes += int.Parse(PckMinutes.SelectedItem.ToString());
+
+                // Write to the file
+                File.WriteAllText(storedFile, workoutMinutes.ToString());
+
+                // Update the GUI
+                LblResults.Text = workoutMinutes.ToString();
+            }
+            else
+            {
+                DisplayAlert("Invalid Input", "Please select minutes", "Close");
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# INFO 1200 
+### Fall 2020
+
+## Participation 16
+
+--- 
+**Code**
+ - [XAML Code](https://github.com/iingles/iiCoinFlip/blob/master/iiCoinFlip/MainPage.xaml)
+ - [MainPage C# Code](https://github.com/iingles/iiCoinFlip/blob/master/iiCoinFlip/MainPage.xaml.cs)
+ - [Coin Class C# Code](https://github.com/iingles/iiCoinFlip/blob/master/iiCoinFlip/Coin.cs)
+
+**Screenshots**
+ - [Main Screen](https://github.com/iingles/iiCoinFlip/blob/master/main-screen.jpg)
+ - [1x Result](https://github.com/iingles/iiCoinFlip/blob/master/1x-result.jpg)
+ - [5x Result](https://github.com/iingles/iiCoinFlip/blob/master/5x-results.jpg)
+
